00f071e4be8a2dcd17cee67cfcb0d6f4455f5031
Mabala-Karabo <mabalakarabo25@gmail.com>
2023-09-18 16:52:44 +0200

    Add project files.

---
+++ TreeView/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        Button1 = New Button()
+        TreeView1 = New Windows.Forms.TreeView()
+        ImageList1 = New ImageList(components)
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI Semibold", 18F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Button1.Location = New Point(12, 12)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(251, 59)
+        Button1.TabIndex = 0
+        Button1.Text = "Add Note"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TreeView1
+        ' 
+        TreeView1.ImageIndex = 0
+        TreeView1.ImageList = ImageList1
+        TreeView1.Location = New Point(153, 114)
+        TreeView1.Name = "TreeView1"
+        TreeView1.SelectedImageIndex = 0
+        TreeView1.Size = New Size(600, 290)
+        TreeView1.TabIndex = 1
+        ' 
+        ' ImageList1
+        ' 
+        ImageList1.ColorDepth = ColorDepth.Depth8Bit
+        ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), ImageListStreamer)
+        ImageList1.TransparentColor = Color.Transparent
+        ImageList1.Images.SetKeyName(0, "Screenshot (145).png")
+        ImageList1.Images.SetKeyName(1, "Screenshot (282).png")
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TreeView1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TreeView1 As Windows.Forms.TreeView
+    Friend WithEvents ImageList1 As ImageList
+End Class
---
+++ TreeView/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    ' this variable allows you to use all those images in image lists one by one
+    Dim i As Integer = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        i = (i + 1) Mod 2 'this two is the number of images inserted in image list but this command allows the loop through those images
+        Dim note As String
+        note = InputBox("Enter a node name") ' to initialize a input box
+        ' to add notes into treeview box using this command TreeView1.Nodes.Add(note, note)
+
+        'add select function into tree view after adding
+        If TreeView1.SelectedNode Is Nothing Then
+            TreeView1.Nodes.Add(note, note, i, i) 'keys passing note as an argument,text as string
+            'the above code allows adding subnotes. o for image index and 1 is for selected image index
+
+        Else
+            TreeView1.SelectedNode.Nodes.Add(note, note, i, i) 'for selection in treeview to allow selection
+        End If
+
+    End Sub
+End Class
